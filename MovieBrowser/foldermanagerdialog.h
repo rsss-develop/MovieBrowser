b111617affdd964e613d175f2d260d2fa98c0296
rsss <xx3223.syozawa@gmail.com>
2020-09-23 12:30:28 +0900

    commit

--- MovieBrowser/foldermanagerdialog.h
+++ MovieBrowser/foldermanagerdialog.h
@@ -2,10 +2,44 @@
 #define FOLDERMANAGERDIALOG_H
 
 #include <QDialog>
+#include <QFileSystemModel>
+#include <QVariant>
+#include <QSet>
 
 namespace Ui {
 class FolderManagerDialog;
 }
+
+//　監視対象フォルダを選択するためのカスタムモデル
+// 先頭に監視対象かどうかのアイコンを表示させる
+class MonitorFileSystemModel : public QFileSystemModel
+{
+    Q_OBJECT
+
+public:
+    explicit MonitorFileSystemModel(QObject *parent = Q_NULLPTR)
+        : QFileSystemModel(parent) {}
+
+    enum Roles  {
+        MonitorRole = Qt::UserRole + 4,
+    };
+    Q_ENUM(Roles)
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE
+    {
+        if (index.isValid() && role >= MonitorRole) {
+            switch (role) {
+            case MonitorRole:
+                // 監視対象かどうかを確認
+                return (monitorList.contains(index)) ? Qt::Checked : Qt::Unchecked;
+            default:
+                break;
+            }
+        }
+        return QFileSystemModel::data(index, role);
+    }
+
+};
 
 class FolderManagerDialog : public QDialog
 {
@@ -17,6 +51,8 @@ public:
 
 private:
     Ui::FolderManagerDialog *ui;
+
+    QSet<QPersistentModelIndex> monitorList;
 };
 
 #endif // FOLDERMANAGERDIALOG_H
